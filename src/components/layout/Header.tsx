992903247df2f2628038a59730229c83ae833926
newpatientflow-ops <newpatientflow@gmail.com>
2026-09-11 09:41:31 +0530

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -44,6 +44,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KK7S6KMT');</script>
 <!-- End Google Tag Manager -->
+  
     </header>
   );
 }
